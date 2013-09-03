22448707cf5f8d5ceaccda3645f3cc4c3f9a2e49
James Tang <mystcolor@gmail.com>
2013-09-03 16:55:00 +0800

    Initial Commit

---
+++ bugsReport/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bugsReport
+//
+//  Created by James Tang on 3/9/13.
+//  Copyright (c) 2013 James Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
